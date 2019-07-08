2cac252948221c6f18c96d2c3b5290a5e2516dc6
Matthias Bruch . netzkern <matthias.bruch@netzkern.de>
2019-07-08 21:33:43 +0200

    Sensors

--- App.tsx
+++ App.tsx
@@ -1,29 +1,48 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import { Pedometer } from 'expo-sensors';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.fontSize}>Hallo Anna2</Text>
+      <Text style={styles.fontSize}>Sienna Aid</Text>
       <Button title="Klick mich! LOS!" onPress={callHome}></Button>
     </View>
   );
 }
 
+Pedometer.watchStepCount(stepArgs => {
+  console.log(stepArgs.steps)
+});
+
+// Pedometer.setUpdateInterval(200);
+// Pedometer.addListener(accelerometerData => {
+//   var data = accelerometerData ;
+//   console.log("x: " + Math.round(data.x) + " | y: " + Math.round(data.y) + " | z: " + Math.round(data.z)); 
+// });
+
+// setTimeout(() => {
+//   // If it's the last subscription to accelerometer it will stop polling in the native API
+//   subscription.unsubscribe();
+// }, 1000);
+
 function callHome() {
-  fetch('https://www.netzkern.de/', {
+  fetch('http://10.70.16.64/api/values', {
     method: 'POST',
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
+      'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      firstParam: 'yourValue',
-      secondParam: 'yourOtherValue',
-    }),
-  }).then(function(response) {
-    alert(JSON.stringify(response));
-  }).catch((error) => {
+      Key: "key2",
+      Value: "value4"
+    })
+  })
+  // .then((response) => response.json())
+  .then((responseJson) => {
+    alert(JSON.stringify(responseJson));
+  })
+  .catch((error) => {
     console.error(error);
   });
 }
