c6bd5c5c524fec2389db293a57ebb1731bb58dc4
Matthias Bruch . netzkern <matthias.bruch@netzkern.de>
2019-09-04 21:39:35 +0200

    Collecting data and sending structured events to hub.

--- App.tsx
+++ App.tsx
@@ -1,63 +1,94 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { Pedometer } from 'expo-sensors';
-
+import { StyleSheet, Text, View, Button, Picker } from 'react-native';
+import { Pedometer, Gyroscope, Accelerometer } from 'expo-sensors';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.fontSize}>Sienna Aid</Text>
-      <Button title="Klick mich! LOS!" onPress={reportEvent}></Button>
+
+      <Picker
+        mode="dropdown"
+        selectedValue={config.expectedEvent}
+        style={{height: 50, width: 100, paddingBottom:200}}
+        onValueChange={(itemValue, itemIndex) => {
+          setExpectedEvent(itemValue)
+        }}
+        >
+        <Picker.Item label="Falling" value="falling" />
+        <Picker.Item label="Walking" value="walking" />
+        <Picker.Item label="Laying" value="laying" />
+      </Picker>
+
+      <Button title="Start events" onPress={startEventTracking}></Button>
+      <Button title="Stop events" onPress={stopEventTracking}></Button>
     </View>
   );
 }
 
-// var x = 0;
-// Pedometer.watchStepCount(stepArgs => {
-//   console.log(stepArgs.steps)
+var config = {
+  expectedEvent: null,
+  trackingStarted: null,
+  trackingEnded: null,
+  sensors: {
+    gyroscope: [],
+    accelerometer: []
+  }
+};
 
-//   x = stepArgs.steps
-// });
+function setExpectedEvent(eventName) {
+  config.expectedEvent = eventName;
+}
 
-var start = new Date();
-setInterval(() => {
-  var end = new Date();
-  Pedometer.getStepCountAsync(start, end).then((stepsArgs) => {
-    reportEvent(stepsArgs.steps);
+function startEventTracking() {
+  config.trackingStarted = new Date();
+
+  startSensors();
+}
+
+function stopEventTracking() {
+  config.trackingEnded = new Date();
+
+  stopSensors();
+}
+
+
+function startSensors() {
+  Gyroscope.addListener(result => {
+    config['sensors']['gyroscope'].push(result);
   });
-  start = end;
-}, 20000);
+  Accelerometer.addListener(result => {
+    config['sensors']['accelerometer'].push(result);
+  });
+}
 
+function stopSensors() {
+  Gyroscope.removeAllListeners();
+  Accelerometer.removeAllListeners();
+  reportToHub();
+}
 
-// Pedometer.setUpdateInterval(200);
-// Pedometer.addListener(accelerometerData => {
-//   var data = accelerometerData ;
-//   console.log("x: " + Math.round(data.x) + " | y: " + Math.round(data.y) + " | z: " + Math.round(data.z)); 
-// });
+function reportToHub() {
+  var request = {
+    Start: convertDateToString(config.trackingStarted),
+    End: convertDateToString(config.trackingEnded),
+    EventType: config.expectedEvent,
+    Accelerometer: config['sensors'].accelerometer,
+    Gyroscope: config['sensors'].gyroscope
+  };
 
-// setTimeout(() => {
-//   // If it's the last subscription to accelerometer it will stop polling in the native API
-//   subscription.unsubscribe();
-// }, 1000);
-
-function reportEvent(stepCount) {
-  fetch('http://10.70.16.64/api/values', {
+  fetch('https://10.70.41.3:5000/api/values', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({
-      Key: "StepCount ",
-      Value: stepCount
-    })
+    body: JSON.stringify(request)
   })
   // .then((response) => response.json())
   .then((responseJson) => {
     alert(JSON.stringify(responseJson));
   })
-  .catch((error) => {
-    console.error(error);
-  });
+  
 }
 
 const styles = StyleSheet.create({
@@ -71,3 +102,36 @@ const styles = StyleSheet.create({
     fontSize: 60
   }
 });
+
+
+function convertDateToString(date) {
+  return date.getFullYear() + "-" + 
+        padLeft(date.getMonth() + 1, 2) + "-" + 
+        padLeft(date.getDate(), 2) + " " + 
+        padLeft(date.getHours(), 2) + ":" + 
+        padLeft(date.getMinutes(), 2) + ":" + 
+        padLeft(date.getSeconds(), 2);
+}
+
+function padLeft(str, length) {
+  str += '';
+
+  while (str.length < length) {
+    str = '0' + str;
+  }
+
+  return str;
+}
+
+
+// var x = 0;
+// Pedometer.watchStepCount(stepArgs => {
+//   console.log(stepArgs.steps)
+//   x = stepArgs.steps
+// });
+
+// Pedometer.setUpdateInterval(200);
+// Pedometer.addListener(accelerometerData => {
+//   var data = accelerometerData ;
+//   console.log("x: " + Math.round(data.x) + " | y: " + Math.round(data.y) + " | z: " + Math.round(data.z)); 
+// });
